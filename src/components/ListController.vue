8c26c3b39324750f633c07272bef9b9d83ae769a
Hosein Mirian <hosein.mirian@gmail.com>
2020-04-17 12:46:17 +0100

    added items to listController

--- src/components/ListController.vue
+++ src/components/ListController.vue
@@ -1,8 +1,32 @@
 <template>
-    <section class="d-flex  ">
+    <section class="d-flex  justify-center">
+        <v-btn-toggle
+                v-model="toggle_exclusive"
+                mandatory
+        >
+            <v-btn>
+                <v-icon>mdi-format-align-left</v-icon>
+            </v-btn>
+        </v-btn-toggle>
+
         <v-pagination v-model="page"
                       :length="5"
         />
+        <v-btn-toggle
+                v-model="toggle_exclusive"
+                mandatory
+        >
+            <v-btn>
+                <v-icon>mdi-format-align-left</v-icon>
+            </v-btn>
+            <v-btn>
+                <v-icon>mdi-format-align-center</v-icon>
+            </v-btn>
+            <v-btn>
+                <v-icon>mdi-format-align-right</v-icon>
+            </v-btn>
+
+        </v-btn-toggle>
     </section>
 </template>
 
