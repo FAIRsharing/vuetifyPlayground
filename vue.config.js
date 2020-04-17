366266410902137bd48e74de9a8a215eabdbcada
Hosein Mirian <hosein.mirian@gmail.com>
2020-04-17 14:54:35 +0100

    min change 2

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,7 @@
 // const path = require("path");
 
 module.exports = {
-    publicPath: './',
+    publicPath: './dist',
     // outputDir: path.resolve(__dirname, "./dist"),
     "transpileDependencies": [
         "vuetify"
