85659bb8d46bff2bc621c0de26b82e91ecbd9aa4
Hosein Mirian <hosein.mirian@gmail.com>
2020-04-17 15:13:23 +0100

    aa

--- dist/service-worker.js
+++ dist/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "vuetifyPlayground/precache-manifest.430d48bc6f7db76a5884e00840b51379.js"
+  "dist/precache-manifest.8df6bb4d77c35347e64ceae44c540477.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "vuetify-cli"});
