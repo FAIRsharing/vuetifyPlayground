824a440d2adfa98fc0a5ffde845e0be1e33eca00
Terazus <batistadominique@hotmail.com>
2020-04-20 04:53:44 -0700

    removing dist from gitignore

---
+++ dist/precache-manifest.11d886137d16643bc376f0714e2177d5.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c33339913d1e5ec1421b",
+    "url": "css/app.f394e3ca.css"
+  },
+  {
+    "revision": "73cbea167d62fda90200",
+    "url": "css/chunk-vendors.bcd3bbb2.css"
+  },
+  {
+    "revision": "0509ab09c1b0d2200a4135803c91d6ce",
+    "url": "fonts/MaterialIcons-Regular.0509ab09.woff2"
+  },
+  {
+    "revision": "29b882f018fa6fe75fd338aaae6235b8",
+    "url": "fonts/MaterialIcons-Regular.29b882f0.woff"
+  },
+  {
+    "revision": "96c476804d7a788cc1c05351b287ee41",
+    "url": "fonts/MaterialIcons-Regular.96c47680.eot"
+  },
+  {
+    "revision": "da4ea5cdfca6b3baab285741f5ccb59f",
+    "url": "fonts/MaterialIcons-Regular.da4ea5cd.ttf"
+  },
+  {
+    "revision": "63d2a5950fc212096c3612f71ae66f64",
+    "url": "fonts/materialdesignicons-webfont.63d2a595.woff"
+  },
+  {
+    "revision": "7fb0e3780372f10f804513e2e0c39e78",
+    "url": "fonts/materialdesignicons-webfont.7fb0e378.woff2"
+  },
+  {
+    "revision": "a1a0ed860f50ffd42ba46d7c10f30255",
+    "url": "fonts/materialdesignicons-webfont.a1a0ed86.eot"
+  },
+  {
+    "revision": "fe1545ef4dd1eef2f1e25528898fc0b3",
+    "url": "fonts/materialdesignicons-webfont.fe1545ef.ttf"
+  },
+  {
+    "revision": "f62185d1a2d410613081455f337e667d",
+    "url": "img/fairsharing-logo.f62185d1.svg"
+  },
+  {
+    "revision": "07d1e22e8cddd99a53037dc510221b7b",
+    "url": "img/logo.07d1e22e.svg"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "ddf7c7743d540c080e31c202a89c0a46",
+    "url": "index.html"
+  },
+  {
+    "revision": "7d24311153ef9fa4f8b8",
+    "url": "js/about.c666870c.js"
+  },
+  {
+    "revision": "c33339913d1e5ec1421b",
+    "url": "js/app.0ac0019a.js"
+  },
+  {
+    "revision": "73cbea167d62fda90200",
+    "url": "js/chunk-vendors.ee53a558.js"
+  },
+  {
+    "revision": "65a26a893471a81549361a477c2fd4af",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "robots.txt"
+  }
+]);
