e531d022ef2816ceb42f7f02a77130ce81cd6ffb
Hosein Mirian <hosein.mirian@gmail.com>
2020-04-22 16:37:55 +0100

    created RecordsCard - Added 3 controller buttons for each item

---
+++ src/components/RecordsCard.vue
@@ -0,0 +1,51 @@
+<template>
+    <v-card
+            class="pa-2 d-flex  align-center"
+            outlined
+            tile
+            height="120px"
+            elevation="3"
+    >
+        <div class="ml-2 pr-6">
+            <circle-holder :status="RecordStatus"/>
+        </div>
+        <h3 class="pl-2 h3-text-color max-height max-width">Record title is a bit long! test {{n}}</h3>
+        <section class="ml-4 d-flex flex-column">
+            <h4 class="d-none">select Tag type</h4>
+            <v-btn v-for="(item,index) in items" :key="index" :outlined="item.active" text class="button-text-color"
+                   :color="item.active?'primary':null" @click="changeActiveItem(index)">{{item.title}}
+            </v-btn>
+        </section>
+    </v-card>
+</template>
+
+<script>
+    import CircleHolder from "./CircleHolder";
+
+    export default {
+        name: "RecordsCard",
+        components: {CircleHolder},
+        props: {
+            RecordStatus: null,
+        },
+        data() {
+            return {
+                active: true,
+                items: [{title: 'SUBJECT', active: false}, {title: 'DOMAIN', active: true}, {
+                    title: 'TAXONOMY',
+                    active: false
+                }]
+            }
+        },
+        methods: {
+            changeActiveItem: function (itemIndex) {
+                this.items.map(item => item.active = false);
+                this.items[itemIndex].active = true;
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
--- src/components/RightContent.vue
+++ src/components/RightContent.vue
@@ -4,38 +4,19 @@
             Result
         </h2>
         <section v-for="n in 30" :key="n" class="pt-2 pt-lg-3">
-            <v-card
-                    class="pa-2 d-flex  align-center"
-                    outlined
-                    tile
-                    height="120px"
-                    elevation="3"
-            >
-                <div class="ml-2 pr-6">
-                    <circle-holder :status="n%2===0?statuses[0]:statuses[1]" :key="n"/>
-                </div>
-                <h3 class="pl-2 h3-text-color max-height max-width">Record title is  a bit long! test {{n}}</h3>
-                <section class="ml-4 d-flex flex-column">
-                    <h4 class="d-none">select Tag type</h4>
-                    <v-btn  text  class="button-text-color">SUBJECT</v-btn>
-                    <v-btn  outlined color="primary">DOMAIN</v-btn>
-                    <v-btn  text  class="button-text-color">TAXONOMY</v-btn>
-                </section>
-            </v-card>
+            <RecordsCard :record-status="n%2===0?'ready':'deprecated'" :key="n"></RecordsCard>
         </section>
     </article>
 </template>
 
 <script>
-    import CircleHolder from "./CircleHolder";
+    import RecordsCard from "./RecordsCard";
 
     export default {
         name: "RightContent",
-        components: {CircleHolder},
+        components: {RecordsCard},
         data() {
-            return {
-                statuses: ['ready', 'deprecated']
-            }
+
         }
     }
 </script>
