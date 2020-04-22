819ac3582ee055d834bb8cd2cebf36cd462a5d42
Hosein Mirian <hosein.mirian@gmail.com>
2020-04-22 15:22:18 +0100

    minor changes to list height

--- src/components/RightContent.vue
+++ src/components/RightContent.vue
@@ -14,7 +14,7 @@
                 <div class="ml-2 pr-6">
                     <circle-holder :status="n%2===0?statuses[0]:statuses[1]" :key="n"/>
                 </div>
-                <h3 class="pl-2 h3-text-color max-height-large max-width">Record title is  a bit long! test {{n}}</h3>
+                <h3 class="pl-2 h3-text-color max-height max-width">Record title is  a bit long! test {{n}}</h3>
                 <section class="ml-4 d-flex flex-column">
                     <h4 class="d-none">select Tag type</h4>
                     <v-btn  text  class="button-text-color">SUBJECT</v-btn>
