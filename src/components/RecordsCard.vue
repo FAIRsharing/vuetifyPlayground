e4cb7b153664440b04f748798c8eff75c31a2b96
Hosein Mirian <hosein.mirian@gmail.com>
2020-04-23 11:10:17 +0100

    chips added

--- src/components/RecordsCard.vue
+++ src/components/RecordsCard.vue
@@ -3,18 +3,27 @@
             class="pa-2 d-flex  align-center"
             outlined
             tile
-            height="120px"
+            height="130px"
             elevation="3"
     >
         <div class="ml-2 pr-6">
             <circle-holder :status="RecordStatus"/>
         </div>
-        <h3 class="pl-2 h3-text-color max-height max-width">Record title is a bit long! test {{n}}</h3>
-        <section class="ml-4 d-flex flex-column">
+        <h3 class="pl-2   max-height max-width" >Record title  as as asas test long {{n}}</h3>
+        <section class="ml-2 mr-4 d-flex flex-column">
             <h4 class="d-none">select Tag type</h4>
-            <v-btn v-for="(item,index) in items" :key="index" :outlined="item.active" text class="button-text-color"
+            <v-btn v-for="(item,index) in buttons" :key="index" :outlined="item.active" text class="button-text-color"
                    :color="item.active?'primary':null" @click="changeActiveItem(index)">{{item.title}}
             </v-btn>
+        </section>
+        <section class="chips-container">
+            <h5 class="d-none">Choose Subject , Domain , Taxonomy  </h5>
+            <v-chip-group
+                    column
+                    multiple
+            >
+                <v-chip small text-color="secondary" color="secondary" v-for="(item,index) in 5" :key="index" filter outlined>{{item}}</v-chip>
+            </v-chip-group>
         </section>
     </v-card>
 </template>
@@ -31,21 +40,26 @@
         data() {
             return {
                 active: true,
-                items: [{title: 'SUBJECT', active: false}, {title: 'DOMAIN', active: true}, {
+                buttons: [{title: 'SUBJECT', active: false}, {title: 'DOMAIN', active: true}, {
                     title: 'TAXONOMY',
-                    active: false
-                }]
+                    active: false,
+                }],
+                // amenities: ['1, 4'],
             }
         },
         methods: {
             changeActiveItem: function (itemIndex) {
-                this.items.map(item => item.active = false);
-                this.items[itemIndex].active = true;
+                this.buttons.map(item => item.active = false);
+                this.buttons[itemIndex].active = true;
             }
         }
     }
 </script>
 
 <style scoped>
-
+    .chips-container{
+        height: 110px;
+        width: 80%;
+        overflow-x: hidden;
+    }
 </style>
