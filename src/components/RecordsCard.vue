593e153730b7fc5c721e09b6ea6d3b66665d6cab
Hosein Mirian <hosein.mirian@gmail.com>
2020-04-23 11:53:45 +0100

    chips structured well to receive actual data

--- src/components/RecordsCard.vue
+++ src/components/RecordsCard.vue
@@ -9,7 +9,7 @@
         <div class="ml-2 pr-6">
             <circle-holder :status="RecordStatus"/>
         </div>
-        <h3 class="pl-2   max-height max-width" >Record title  as as asas test long {{n}}</h3>
+        <h3 class="pl-2   max-height max-width">Record title as as asas test long {{n}}</h3>
         <section class="ml-2 mr-4 d-flex flex-column">
             <h4 class="d-none">select Tag type</h4>
             <v-btn v-for="(item,index) in buttons" :key="index" :outlined="item.active" text class="button-text-color"
@@ -17,12 +17,16 @@
             </v-btn>
         </section>
         <section class="chips-container">
-            <h5 class="d-none">Choose Subject , Domain , Taxonomy  </h5>
+            <h5 class="d-none">Choose Subject , Domain , Taxonomy </h5>
             <v-chip-group
                     column
                     multiple
             >
-                <v-chip small text-color="secondary" color="secondary" v-for="(item,index) in 5" :key="index" filter outlined>{{item}}</v-chip>
+                <v-chip small text-color="secondary" color="secondary" v-for="(chip,index) in Chips[currentActiveChips]"
+                        :key="index"
+                        filter
+                        outlined @click="toggleChipActiveness(chip)">{{chip.title}}
+                </v-chip>
             </v-chip-group>
         </section>
     </v-card>
@@ -40,24 +44,47 @@
         data() {
             return {
                 active: true,
-                buttons: [{title: 'SUBJECT', active: false}, {title: 'DOMAIN', active: true}, {
-                    title: 'TAXONOMY',
+                buttons: [{title: 'SUBJECTS', active: false}, {title: 'DOMAINS', active: true}, {
+                    title: 'TAXONOMIES',
                     active: false,
                 }],
-                // amenities: ['1, 4'],
+                Chips: {
+                    SUBJECTS: [
+                        {title: 'subject-Chip1', active: false}, {title: 'subject-Chip2', active: false},
+                    ],
+                    DOMAINS: [
+                        {title: 'domain-Chip1', active: false}, {title: 'domain-Chip2', active: false},
+                        {title: 'domain-Chip3', active: false}, {title: 'domain-Chip4', active: false},
+                    ],
+                    TAXONOMIES: [
+                        {title: 'taxonomies-Chip1', active: false}, {title: 'taxonomies-Chip2', active: false},
+                        {title: 'taxonomies-Chip3', active: false}, {title: 'taxonomies-Chip4', active: false},
+                        {title: 'taxonomies-Chip5', active: false}, {title: 'taxonomies-Chip6', active: false},
+                        {title: 'taxonomies-Chip7', active: false}, {title: 'taxonomies-Chip8', active: false},
+                        {title: 'taxonomies-Chip9', active: false}, {title: 'taxonomies-Chip10', active: false},
+                        {title: 'taxonomies-Chip11', active: false}, {title: 'taxonomies-Chip12', active: false},
+                    ],
+                },
+                currentActiveChips: 'DOMAINS'
             }
         },
         methods: {
             changeActiveItem: function (itemIndex) {
                 this.buttons.map(item => item.active = false);
                 this.buttons[itemIndex].active = true;
+                // changing currentChips data
+                this.currentActiveChips = this.buttons[itemIndex].title;
+            },
+            toggleChipActiveness: function (itemIndex) {
+                let selectedItem = this.items.find(item => item === itemIndex);
+                this.items.map(item => item === selectedItem ? item.active = !item.active : item.active = true);
             }
         }
     }
 </script>
 
 <style scoped>
-    .chips-container{
+    .chips-container {
         height: 110px;
         width: 80%;
         overflow-x: hidden;
