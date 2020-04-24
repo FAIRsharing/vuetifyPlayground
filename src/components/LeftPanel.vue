a36827e33ae7ccd7332e5dd360f9db8a5724a2db
Hosein Mirian <hosein.mirian@gmail.com>
2020-04-24 13:56:19 +0100

    filter panel structuring on the way

--- src/components/LeftPanel.vue
+++ src/components/LeftPanel.vue
@@ -1,30 +1,75 @@
 <template>
     <aside>
-        <v-sheet color="orange lighten-2 ">
-            <h2 class="indigo--text text--lighten-2 ">
+        <v-card id="filters-holder"
+                class="pa-2"
+                outlined
+                tile
+                elevation="3"
+        >
+            <h2 class="d-none">
                 Filter List
             </h2>
-            <div v-for="n in 200" style="background-color: gray" :key="n">filterItem {{n}}</div>
-            <div>Last</div>
-        </v-sheet>
+
+            <v-list dense>
+                <v-list-item-group v-model="item" color="primary">
+                    <v-list-item
+                            v-for="(item, i) in items"
+                            :key="i"
+                    >
+                        <v-list-item-icon>
+                            <v-icon v-text="item.icon"></v-icon>
+                        </v-list-item-icon>
+                        <v-list-item-content>
+                            <v-list-item-title v-text="item.text"></v-list-item-title>
+                        </v-list-item-content>
+                    </v-list-item>
+                </v-list-item-group>
+            </v-list>
+
+        </v-card>
     </aside>
 </template>
 
 <script>
     export default {
-        name: "LeftPanel"
+        name: "LeftPanel",
+        data: () => ({
+            item: 3,
+            items: [
+                { text: 'Real-Time', icon: 'mdi-clock' },
+                { text: 'Audience', icon: 'mdi-account' },
+                { text: 'Conversions', icon: 'mdi-flag' },
+                { text: 'Real-Time', icon: 'mdi-clock' },
+                { text: 'Audience', icon: 'mdi-account' },
+                { text: 'Conversions', icon: 'mdi-flag' },
+                { text: 'Real-Time', icon: 'mdi-clock' },
+                { text: 'Audience', icon: 'mdi-account' },
+                { text: 'Conversions', icon: 'mdi-flag' },
+                { text: 'Real-Time', icon: 'mdi-clock' },
+                { text: 'Audience', icon: 'mdi-account' },
+                { text: 'Conversions', icon: 'mdi-flag' },
+                { text: 'Real-Time', icon: 'mdi-clock' },
+                { text: 'Audience', icon: 'mdi-account' },
+                { text: 'Conversions', icon: 'mdi-flag' },
+                { text: 'Real-Time', icon: 'mdi-clock' },
+                { text: 'Audience', icon: 'mdi-account' },
+                { text: 'Conversions', icon: 'mdi-flag' },
+
+            ],
+        }),
     }
 </script>
 
 <style scoped lang="scss">
+
     aside {
+        #filters-holder {
+            border-radius: 0;
+            -moz-border-radius: 0;
+            -webkit-border-radius: 0;
+            overflow-x: hidden;
+            height: 100vh;
+        }
     }
 
-    .v-sheet {
-        border-radius: 0;
-        -moz-border-radius: 0;
-        -webkit-border-radius: 0;
-        overflow-x: hidden;
-        height: 100vh;
-    }
 </style>
