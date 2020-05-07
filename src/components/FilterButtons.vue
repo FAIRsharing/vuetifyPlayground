d08b3c899fda434e5ba2d999f637aa0976a96474
Hosein Mirian <hosein.mirian@gmail.com>
2020-05-07 09:13:36 +0100

    expansion panel moved to filterButtons

--- src/components/FilterButtons.vue
+++ src/components/FilterButtons.vue
@@ -28,6 +28,26 @@
                 <span>{{item.toolTip}}</span>
             </v-tooltip>
         </div>
+        <v-expansion-panels
+                v-model="panel"
+                multiple
+                flat
+                hover
+                accordion
+        >
+            <v-expansion-panel
+                    v-for="(item,i) in items"
+                    :key="i"
+            >
+                <v-expansion-panel-header>Header {{ item }}</v-expansion-panel-header>
+                <v-expansion-panel-content>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+                    labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                    laboris nisi ut aliquip ex ea commodo consequat.
+                </v-expansion-panel-content>
+            </v-expansion-panel>
+        </v-expansion-panels>
+
     </div>
 </template>
 
@@ -37,6 +57,8 @@
         props: {mdScreens: null},
         data() {
             return {
+                panel: [],
+                items: 20,
                 buttonsGroup: [
                     [{title: 'ALL', active: true}, {title: 'RECOMMENDED', active: false}, {
                         title: 'NOT RECOMMENDED',
@@ -72,12 +94,26 @@
             selectFilter: function (index, selectedButtonsArray) {
                 selectedButtonsArray.map(item => item.active = false);
                 selectedButtonsArray[index].active = true;
+            },
+            // Create an array the length of our items
+            // with all values as true
+            all() {
+                this.panel = [...Array(this.items).keys()].map((k, i) => i)
+            },
+            // Reset the panel
+            none() {
+                this.panel = []
             }
         },
         computed: {
             getRecordStatus: function () {
                 return this.statusStyles['ready'];
             }
+        },
+        created() {
+            //open first expandable panel.
+            this.panel['0'] = 0;
+            // this.all();
         }
     }
 </script>
--- src/components/LeftPanel.vue
+++ src/components/LeftPanel.vue
@@ -10,25 +10,6 @@
                 Filter List
             </h2>
             <FilterButtons :md-screens="$vuetify.breakpoint.mdAndDown"/>
-            <v-expansion-panels
-                    v-model="panel"
-                    multiple
-                    flat
-                    hover
-                    accordion
-            >
-                <v-expansion-panel
-                        v-for="(item,i) in items"
-                        :key="i"
-                >
-                    <v-expansion-panel-header>Header {{ item }}</v-expansion-panel-header>
-                    <v-expansion-panel-content>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
-                        labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
-                        laboris nisi ut aliquip ex ea commodo consequat.
-                    </v-expansion-panel-content>
-                </v-expansion-panel>
-            </v-expansion-panels>
         </v-card>
     </aside>
 </template>
@@ -38,10 +19,6 @@
     export default {
         name: "LeftPanel",
         components: {FilterButtons},
-        data: () => ({
-            panel: [],
-            items: 5,
-        }),
         created() {
             //open first expandable panel.
             this.panel['0'] = 0;
