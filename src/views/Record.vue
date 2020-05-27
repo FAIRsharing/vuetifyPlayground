5067275b4669a4fa493226fd71540385630bd0f5
hosein_mirian <hosein.mirian@gmail.com>
2020-05-27 11:47:20 +0100

    added title and status

--- src/views/Record.vue
+++ src/views/Record.vue
@@ -9,40 +9,54 @@
         <v-container fluid
         >
             <!--  Content  -->
-            <v-row no-gutters
+            <v-row no-gutters class=" "
             >
-                <v-col cols="12" lg="12" md="12" xl="12" class="mt-2 ml-2">
-                    <div>general info</div>
-                    <div>general info</div>
-                    <div>general info</div>
-                    <div>general info</div>
-                    <div>general info</div>
-                    <div>general info</div>
-                    <div>general info</div>
-                    <div>general info</div>
-                    <div>general info</div>
-                    <div>general info</div>
-                    <div>general info</div>
-                    <div>general info</div>
-                    <div>general info</div>
-                    <div>general info</div>
-                    <div>general info</div>
-                    <div>general info</div>
-                    <div>general info</div>
-                    <div>general info</div>
-                    <div>general info</div>
-                    <div>general info</div>
-                    <div>general info</div>
-                    <div>general info</div>
-                    <div>general info</div>
-                    <div>general info</div>
-                    <div>general info</div>
-                    <div>general info</div>
-                    <div>general info</div>
-                    <div>general info</div>
-                    <div>general info</div>
-                    <div>general info</div>
-                    <div>general info</div>
+                <v-col cols="12" lg="12" md="12" xl="12">
+                    <v-card
+                            class="pa-2"
+                            outlined
+                            tile
+                            elevation="1"
+                    >
+                        <Ribbon title="RECOMMENDED" v-if="true"></Ribbon>
+                        <div class="d-flex flex-row align-center">
+                            <circle-holder status="ready" class="mr-8"/>
+                            <h2>Record title example</h2>
+                        </div>
+                        <div>general info</div>
+                        <div>general info</div>
+                        <div>general info</div>
+                        <div>general info</div>
+                        <div>general info</div>
+                        <div>general info</div>
+                        <div>general info</div>
+                        <div>general info</div>
+                        <div>general info</div>
+                        <div>general info</div>
+                        <div>general info</div>
+                        <div>general info</div>
+                        <div>general info</div>
+                        <div>general info</div>
+                        <div>general info</div>
+                        <div>general info</div>
+                        <div>general info</div>
+                        <div>general info</div>
+                        <div>general info</div>
+                        <div>general info</div>
+                        <div>general info</div>
+                        <div>general info</div>
+                        <div>general info</div>
+                        <div>general info</div>
+                        <div>general info</div>
+                        <div>general info</div>
+                        <div>general info</div>
+                        <div>general info</div>
+                        <div>general info</div>
+                        <div>general info</div>
+                        <div>general info</div>
+
+                    </v-card>
+
                 </v-col>
             </v-row>
         </v-container>
@@ -51,10 +65,12 @@
 
 <script>
     import JumpTop from "../components/jumpToTop";
+    import Ribbon from "../components/Ribbon";
+    import CircleHolder from "../components/CircleHolder";
 
     export default {
         name: "Record",
-        components: {JumpTop},
+        components: {CircleHolder, Ribbon, JumpTop},
         data: () => {
             return {
                 showScrollToTopButton: false
